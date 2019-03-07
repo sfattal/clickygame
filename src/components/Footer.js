96f25f7f3cb8d9b725a48921739dbb253613f192
sfattal <fattal.sherif@gmail.com>
2019-03-07 01:15:08 -0500

    css styling, cleanup functions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
     <footer className="footer">
         <div className="bottom">
-            Premier League Memory Game <img alt="epl" src="../assets/images/logo.png"/>
+            Premier League Memory Game <img alt="epl" src="../images/logo.png"/>
         </div>
     </footer>
 )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const Navbar = (props) => (
     <nav className="navbar">
-            <span className="brand"><a href="/"><img alt="epl" src="../assets/images/logo.png"/></a></span>
-            <span className="">{props.message}</span>
-            <span>Score: {props.correct} | Top Score: {props.topscore}</span>
+            <span className="brand">
+                <a href="/"><img alt="epl" src="../images/logo.png"/></a>
+            </span>
+
+            <span className="">
+                {props.message}
+            </span>
+
+            <span>
+                Score: {props.correct} | Top Score: {props.topscore}
+            </span>
     </nav>
 )
 export default Navbar;
